c19bf9f264fac87ce1756bef13a8551245739f0b
Jannis Teunissen <jannis@teunissen.net>
2020-02-04 16:32:32 +0100

    Add Makefile and rename

--- example.f90
+++ example.f90
@@ -7,7 +7,7 @@ program example
   integer            :: hash
 
   if (command_argument_count() /= 1) &
-       error stop "Give a string as argument"
+       stop "Give a string as argument"
 
   call get_command_argument(1, string)
   call MurmurHash3_x86_32(string, len_trim(string), 42, hash)
