4c358959a66b35481d1bcdc1fc76365d6639303a
Jannis Teunissen <jannis@teunissen.net>
2025-08-25 22:50:16 +0200

    Add memory bandwidth

--- benchmark.f90
+++ benchmark.f90
@@ -6,8 +6,8 @@ program benchmark
 
   character(len=2**15) :: string
   integer              :: n, length, hash_32, hash_128(4)
-  real(real64)         :: t0, t1
-  integer, parameter   :: n_max = 10*1000*1000
+  real(real64)         :: t0, t1, gbs
+  integer, parameter   :: n_max = 100*1000*1000
 
   if (command_argument_count() /= 1) &
        stop "Give string as argument"
@@ -24,15 +24,21 @@ program benchmark
      call MurmurHash3_x86_32(string, length, 42, hash_32)
   end do
   call cpu_time(t1)
-  write(*, "(A,Z9,E12.4,A)") "x86_32: ", hash_32, &
-       n_max/(t1-t0), " hashes/s"
+
+  ! Determine throughput in GB/s
+  gbs = length * 0.5_real64**30 * n_max / (t1-t0)
+
+  write(*, "(A,Z9,E10.2,A,E10.2,A)") "x86_32: ", hash_32, &
+       n_max/(t1-t0), " hashes/s", gbs, " GB/s"
 
   call cpu_time(t0)
   do n = 1, n_max
      call MurmurHash3_x64_128(string, length, 42, hash_128)
   end do
   call cpu_time(t1)
-  write(*, "(A,4Z9,E12.4,A)") "x64_128:", hash_128, &
-       n_max/(t1-t0), " hashes/s"
+
+  gbs = length * 0.5_real64**30 * n_max / (t1-t0)
+  write(*, "(A,4Z9,E10.2,A,E10.2,A)") "x64_128:", hash_128, &
+       n_max/(t1-t0), " hashes/s", gbs, " GB/s"
 
 end program benchmark
